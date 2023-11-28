3ec4b71e2c37a755ba941bba3f7411e2f02343a5
bunyaminmrcn <bunyaminmrcn@gmail.com>
2023-11-28 16:53:57 +0300

    Mongo docker-compose setup and initialize mongoose

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const helmet = require('helmet')
 const morgan = require('morgan');
-const { json, urlencoded} = express;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+
+const { json, urlencoded } = express;
+dotenv.config();
 
 const app = express();
 
@@ -9,8 +14,11 @@ app.use(helmet());
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(morgan('combined'));
+mongoose.connect(process.env.ME_CONFIG_MONGODB_URL)
+  .then(() => console.log('Connected!'));
+
 
 const PORT = process.env.PORT || 3090;
 app.listen(PORT, () => {
-    console.log('app listening on port' + PORT);
+  console.log('app listening on port' + PORT);
 })
