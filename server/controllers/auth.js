60a39ce00fdd97cf4b933c8a67d08ec0d8716b14
bunyaminmrcn <bunyaminmrcn@gmail.com>
2023-11-28 19:55:43 +0300

    user password encrypt

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,23 @@
-exports.signUp = (req, res, next) => {
-    return res.json({ success: true})
+const User = require('../models/user');
+
+
+exports.signUp = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+        return res.status(422).json({ success: false, msg: 'Email amd/or required' })
+    }
+    const record = await User.findOne({ email });
+    if (record) {
+        return res.json({ success: false, msg: 'Email in use!' })
+    } else {
+
+        const newRecord = new User({
+            email,
+            password
+        })
+        await newRecord.save();
+        return res.json({ success: true })
+    }
+
 }
--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const bcrypt = require('bcrypt')
 //Define our model
 
 const userSchema = new Schema({
@@ -8,6 +8,21 @@ const userSchema = new Schema({
     password: String
 })
 
+userSchema.pre('save', function(next) {
+    // get access to user model
+    const user = this;
+    console.log({ user })
+    // generate a salt then run callback
+    bcrypt.genSalt(10, function(err, salt) {
+        if(err) { return next(err)}
+
+        //hash (encrypt) our passwprd using the salt
+        bcrypt.hash(user.password, salt).then(hash => {
+            user.password = hash;
+            next();
+        }).catch(err => next(err))
+    })
+})
 //Create the model class
 const ModelClass = mongoose.model('user', userSchema);
 
