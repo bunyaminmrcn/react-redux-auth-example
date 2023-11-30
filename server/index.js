c663f9a583c1b1255f67fe296dee91bb690b3f1e
bunyaminmrcn <bunyaminmrcn@gmail.com>
2023-11-30 11:33:38 +0300

    CORS setup

--- server/index.js
+++ server/index.js
@@ -3,13 +3,27 @@ const helmet = require('helmet')
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
+const cors = require('cors');
 
 const { json, urlencoded } = express;
 dotenv.config();
 
 const app = express();
 
+
+var whitelist = [process.env.STAGE == 'dev' ? 'http://localhost:3000' : process.env.DOMAIN]
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+ 
+
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(json());
 app.use(urlencoded({ extended: false }));
